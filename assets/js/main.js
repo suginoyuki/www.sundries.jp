de328051dedcfe93dc50bdc782b0b855a5ee8a5a
suginoyuki <s_yuki_0821@icloud.com>
2024-04-03 00:51:22 +0900

    main.js調整

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,9 +3,9 @@ window.addEventListener('scroll', function () {
   const elm = document.querySelector('.l-header')
   const scroll = window.pageYOffset
   if (scroll > 700) {
-    elm.style.opacity = '1'
+    elm.style.opacity = '1';
   } else {
-    elm.style.opacity = '0'
+    elm.style.opacity = '0';
   }
 });
 
